1879f0beab74c53c9c8416d62ddb0eb49c66bdac
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-06-26 19:05:22 -0300

    fix: took off comments lines of http exception on the server file

--- src/server.ts
+++ src/server.ts
@@ -16,18 +16,18 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-// app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
-//   if (err instanceof HttpException) {
-//     return response.status(err.statusCode).json({
-//       status: 'error',
-//       message: err.message,
-//     });
-//   }
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof HttpException) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
 
-//   return response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-//     status: 'error',
-//     message: 'Internal server error.',
-//   });
-// });
+  return response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    status: 'error',
+    message: 'Internal server error.',
+  });
+});
 
 app.listen(3333, () => console.log('Server running 🚀'));
